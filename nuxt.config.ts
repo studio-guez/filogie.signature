c9af693ea6ca672216b338b2343f6d6fe4d8b0d2
azertypow <nico@villa1203.ch>
2025-01-30 09:39:26 +0100

    chore: nuxt config compatytility update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,14 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   ssr: false,
+
   app: {
     baseURL: '/lecinematographe.signature/',
   },
+
   css: [
     "@/assets/___main.scss",
   ],
+
+  compatibilityDate: "2025-01-30",
 })
